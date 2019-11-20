11a6b434735c106259de4976abd5b649d5c84709
w1ck3dg0ph3r <w1ck3dg0ph3r@gmail.com>
2019-11-20 10:06:51 +0300

    Use gunicorn

--- start.sh
+++ start.sh
@@ -43,7 +43,7 @@ else
     python3 manage.py collectstatic --noinput
 fi
 
-python3 manage.py runserver 127.0.0.1:8000 > /dev/stdout 2> /dev/stderr &
+gunicorn --workers 4 --timeout 60 -b 127.0.0.1:8000 taiga.wsgi > /dev/stdout 2> /dev/stderr &
 TAIGA_PID=$!
 
 mkdir /run/nginx
